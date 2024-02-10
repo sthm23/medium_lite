807d1a97dd3c8435ce06518880a084bbde704600
sthm23 <sanjikk@mail.ru>
2024-02-10 16:51:15 +0500

    feat:add prisma orm and postgres docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
